fa6b766a388382c672580ecf7214e105a1e5c11d
Edward <vergaremail@gmail.com>
2023-09-21 19:34:49 +0800

    Day1

--- 3.FirstSteps/3.2FirstCppProgram/main.cpp
+++ 3.FirstSteps/3.2FirstCppProgram/main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 
 int main(){
-
-    std::cout << "Number1" << std::endl;
-    std::cout << "Number2" << std::endl;
-    std::cout << "Number3" << std::endl;
-
-    return 0;
+    std::cout <<"Number1";
+    std::cout <<"Number2";
 }
