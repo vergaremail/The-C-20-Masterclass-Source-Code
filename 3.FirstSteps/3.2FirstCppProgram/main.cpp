ace2b7cf6105cbf7480083168a3bf9ceafcd2c40
EcodeEcode <94364254+EcodeEcode@users.noreply.github.com>
2023-09-21 20:41:00 +0800

    Day 1

--- 3.FirstSteps/3.2FirstCppProgram/main.cpp
+++ 3.FirstSteps/3.2FirstCppProgram/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 int main(){
-    std::cout <<"Number1";
-    std::cout <<"Number2";
+    std::cout <<"Number1" << std::endl;
+    std::cout <<"Number2" << std::endl;
+    std::cout <<"Number3" << std::endl;
+
+    return 0;
 }
--- 3.FirstSteps/3.3Comments/main.cpp
+++ 3.FirstSteps/3.3Comments/main.cpp
@@ -1,19 +1,17 @@
 //One line comment
-
 //This brings in the iostream library
 #include <iostream>
 
 /*
-    This is a block comment that englobes multiple
-    lines of text
+ This is a block comment that englobes multiple
+ lines of text
+Block comments cannot be nested
 */
-
-
 
 
 int main(){
     //This is going to print "Hello World" to the console
-    std::cout << "Hello World!" << std::endl;
+    std::cout <<"Hello World!" << std::endl;
     return 0;
     //Program ends here
 }
